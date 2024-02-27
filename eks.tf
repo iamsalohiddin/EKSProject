ecd9d3d0802dc51e861e5575227f0f6752ed8680
Salokhiddin Samariddinov <salokhiddin@SalokhiddinsAir.attlocal.net>
2024-02-26 21:37:35 -0600

    Updating files

--- eks.tf
+++ eks.tf
@@ -24,7 +24,7 @@ resource "aws_eks_cluster" "ProjectEKS" {
 
 resource "aws_eks_node_group" "EKS_Node_Group" {
   cluster_name = aws_eks_cluster.ProjectEKS.name
-  node_group_name = "EKS Group"
+  node_group_name = "EKSGroup"
   node_role_arn = aws_iam_role.node-example.arn
   subnet_ids = aws_subnet.private[*].id
   scaling_config {
@@ -39,7 +39,9 @@ resource "aws_eks_node_group" "EKS_Node_Group" {
     name = aws_launch_template.ProjectLT.name
     version = aws_launch_template.ProjectLT.latest_version
   }
-  depends_on = [ aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+  depends_on = [ 
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly ]
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly 
+    ]
 }
